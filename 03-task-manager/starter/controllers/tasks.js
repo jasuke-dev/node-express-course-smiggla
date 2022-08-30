8574a991c18dfec898637b1037f9baa1b4ba5eb9
jasuke-dev <ilham.suryaega@gmail.com>
2022-08-30 21:54:32 +0700

    delete and update api

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -34,11 +34,41 @@ const getTask = async (req,res)=>{
     res.status(500).json({error})
   }
 }
-const updateTask = (req,res)=>{
-  res.send('update tasks')
+const deleteTask = async (req,res)=>{
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findByIdAndDelete({_id:taskID})
+    if (!task) {
+      return res.status(404).json({msg:`No task with id : ${taskID}`})
+    }
+    res.status(200).send()
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
+  
 }
-const deleteTask = (req,res)=>{
-  res.send('delete tasks')
+
+const updateTask = async (req,res)=>{
+  try {
+  const {id:taskID} = req.params;
+  //by default akan mengembalikan nilai lama
+  // const task = await Task.findByIdAndUpdate({_id: taskID},req.body)
+
+  const task = await Task.findByIdAndUpdate({_id: taskID},req.body,{
+    new: true,
+    runValidators: true
+  })
+  
+  if (!task) {
+    return res.status(404).json({msg:`No task with id : ${taskID}`})
+  }
+  res.status(200).json({task})
+
+  res.status(200).json({id:taskID, data: req.body})
+  } catch (error) {
+    
+  }
+  
 }
 
 
