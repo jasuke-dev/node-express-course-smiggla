be2160b3485f60fbbf6844d5619b17dadaa810d3
jasuke-dev <ilham.suryaega@gmail.com>
2022-08-31 11:26:43 +0700

    custom 4o4 route

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,6 +4,8 @@ const taskRoutes = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
+const notFound = require('./middleware/not-found')
+
 const port = 5000;
 
 
@@ -13,6 +15,9 @@ app.use(express.json())
 app.use(express.static('./public'))
 
 app.use('/api/v1/tasks', taskRoutes)
+
+//default jika route tidak ada
+app.use(notFound)
 
 const start = async()=>{
   try{
