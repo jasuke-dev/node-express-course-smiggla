b20fe286dc0384fb085df6b4f1cba6c2cc9ef519
jasuke-dev <ilham.suryaega@gmail.com>
2022-08-31 12:05:40 +0700

    custom async wrapper to right away try catch on ecery controllers

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,55 +1,36 @@
 const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/asyncWrapper')
 
 
+const getAllTasks = asyncWrapper(async (req,res)=>{
+  const tasks = await Task.find({})
+  res.status(200).json({tasks})
+})
 
-const getAllTasks = async (req,res)=>{
-  try {
-    const tasks = await Task.find({})
-    res.status(200).json({tasks})
-  } catch (error) {
-    res.status(500).json({error})
+const createTasks = asyncWrapper(async (req,res)=>{
+  const task = await Task.create(req.body)
+  res.status(201).json({task})
+})
+
+const getTask = asyncWrapper(async (req,res)=>{
+  //deconstruktor dengan alias
+  const {id:taskID} = req.params
+  const task = await Task.findOne({_id:taskID})
+  if(!task){
+    return res.status(404).json({msg:`No task with id : ${taskID}`})
   }
-}
-
-const createTasks = async (req,res)=>{
-  try {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-  } catch (error) {
-    res.status(500).json({error})
+  res.status(200).json({task})
+})
+const deleteTask = asyncWrapper(async (req,res)=>{
+  const {id:taskID} = req.params
+  const task = await Task.findByIdAndDelete({_id:taskID})
+  if (!task) {
+    return res.status(404).json({msg:`No task with id : ${taskID}`})
   }
+  res.status(200).send()
+})
 
-}
-
-const getTask = async (req,res)=>{
-  try {
-    //deconstruktor dengan alias
-    const {id:taskID} = req.params
-    const task = await Task.findOne({_id:taskID})
-    if(!task){
-      return res.status(404).json({msg:`No task with id : ${taskID}`})
-    }
-    res.status(200).json({task})
-  } catch (error) {
-    res.status(500).json({error})
-  }
-}
-const deleteTask = async (req,res)=>{
-  try {
-    const {id:taskID} = req.params
-    const task = await Task.findByIdAndDelete({_id:taskID})
-    if (!task) {
-      return res.status(404).json({msg:`No task with id : ${taskID}`})
-    }
-    res.status(200).send()
-  } catch (error) {
-    res.status(500).json({msg: error})
-  }
-  
-}
-
-const updateTask = async (req,res)=>{
-  try {
+const updateTask = asyncWrapper(async (req,res)=>{
   const {id:taskID} = req.params;
   //by default akan mengembalikan nilai lama
   // const task = await Task.findByIdAndUpdate({_id: taskID},req.body)
@@ -63,12 +44,7 @@ const updateTask = async (req,res)=>{
     return res.status(404).json({msg:`No task with id : ${taskID}`})
   }
   res.status(200).json({task})
-
-  } catch (error) {
-    res.status(500).json({msg: error})
-  }
-  
-}
+})
 
 
 module.exports = {
