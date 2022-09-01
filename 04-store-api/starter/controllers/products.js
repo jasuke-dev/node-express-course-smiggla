982cad908f83a5635f5167f621dac1b5fb32e91c
jasuke-dev <ilham.suryaega@gmail.com>
2022-09-01 16:04:53 +0700

    page limit and skip

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,44 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res)=>{
-  const products =await Product.find({featured:true})
+  const search = 'high'
+  const products =await Product.find({}).select('name price').limit(5)
   res.status(200).json({products})
 }
 
 const getAllProducts = async (req,res)=>{
-  res.status(200).json({msg:'product route'})
+  const {featured, company, name, sort, fields} = req.query
+  const queryObject = {}
+  if(featured){
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if(company){
+    queryObject.company = company
+  }
+  if(name){
+    queryObject.name = { $regex: name, $options:'i'}
+  }
+  let result = Product.find(queryObject)
+  if(sort){
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }else{
+    result = result.sort('createdAt')
+  }
+
+  if(fields){
+    const fieldList = fields.split(',').join(' ')
+    result = result.select(fieldList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page-1) *limit
+
+  result = result.skip(skip).limit(limit)
+
+  const products = await result
+  res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
