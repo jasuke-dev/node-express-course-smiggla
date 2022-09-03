0e411e5ed4bf3440a5f0e7cb7cc38e821fefd979
jasuke-dev <ilham.suryaega@gmail.com>
2022-09-03 19:41:25 +0700

    refactor to middleware

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -7,36 +7,22 @@ const login = async (req,res)=>{
     throw new CustomAPIError('Please provide username and password',400)
   }
   const id = new Date().getDate()
-
+  
   const token = jwt.sign(
     {id,username},
     process.env.JWT_SECRET,
     {expiresIn:'30d'}
     )
-
+    
   res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req,res)=>{
-  const authHeader = req.headers.authorization
-
-  if(!authHeader || !authHeader.startsWith('Bearer')){
-    throw new CustomAPIError('No token Provided',401)
-  }
-
-  const token = authHeader.split(' ')[1]
-
-  try {
-    const decoded = jwt.verify(token,process.env.JWT_SECRET)
-    
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({
-      msg:`Hello, ${decoded.username}`, 
-      secret: `Here is youre authorized data, youre lucky number is ${luckyNumber}`
-    })
-  } catch (error) {
-    throw new CustomAPIError('Not authorized to access this route',401)
-  }
+  const luckyNumber = Math.floor(Math.random()*100)
+  res.status(200).json({
+    msg:`Hello, ${req.user.username}`, 
+    secret: `Here is youre authorized data, youre lucky number is ${luckyNumber}`
+  })
 
 }
 
