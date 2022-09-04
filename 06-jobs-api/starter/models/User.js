1d12bd0af9c0eb911f3e2509567b805b70f6080f
jasuke-dev <ilham.suryaega@gmail.com>
2022-09-04 21:03:58 +0700

    setup User Schema / model

---
+++ 06-jobs-api/starter/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name : {
+    type : String,
+    required : [true, 'Please provide name'],
+    minLength : 3,
+    maxLength : 20,
+  },
+  email : {
+    type : String,
+    required : [true, 'Please provide email'],
+    match : [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please Provide valid email"
+    ],
+    unique : true,
+  },
+  password : {
+    type : String,
+    required : [true, 'Please provide Password'],
+    minLength : 3,
+  },
+  
+})
+
+module.exports = mongoose.model('User',UserSchema)
