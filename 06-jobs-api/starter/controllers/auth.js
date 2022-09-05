e89e330e559301a6475fb603bc84a279070387ff
jasuke-dev <ilham.suryaega@gmail.com>
2022-09-05 22:12:56 +0700

    setup auth middleware to verify JWT

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -17,8 +17,12 @@ const login = async (req,res) =>{
   }
 
   const user = await User.findOne({email})
-  //compare password
   if(!user){
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  //compare password
+  const isPasswordCorrect = await user.comparePassword(password)
+  if(!isPasswordCorrect){
     throw new UnauthenticatedError('Invalid Credentials')
   }
   
