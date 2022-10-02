b3c65f4ebc5c2efe6c22f672d363e3163889bc74
jasuke-dev <ilham.suryaega@gmail.com>
2022-10-02 19:32:42 +0700

    file uploud finish kurang cloudinary

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,22 @@
 const {StatusCodes} = require('http-status-codes')
 const path = require('path')
+const CustomError = require('../errors')
 
 const uploudProductImage = async(req,res) => {
+  if(!req.files){
+    throw new CustomError.BadRequestError('No File Uplouded')
+  }
   const productImage = req.files.image
+  console.log(productImage);
+  if(!productImage.mimetype.startsWith('image')){
+    throw new CustomError.BadRequestError('please uplouad image')
+  }
+  
+  const maxSize = 1024 * 1024
+  
+  if(productImage.size > maxSize){
+    throw new CustomError.BadRequestError('please upload image under 1kb')
+  }
 
   const imagePath = path.join(__dirname,'../public/uploads/'+`${productImage.name}`)
   
