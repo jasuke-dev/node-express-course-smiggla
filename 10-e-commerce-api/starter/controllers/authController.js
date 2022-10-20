c39a4b8bdb0828669c7a8da57253589651e5f7c2
jasuke-dev <ilham.suryaega@gmail.com>
2022-10-20 22:09:58 +0700

    jwt token

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
+
 
 const register = async (req, res) => {
   const {email, name, password} = req.body
@@ -13,7 +16,10 @@ const register = async (req, res) => {
   // first 
 
   const user = await User.create({name, email, password})
-  res.status(StatusCodes.CREATED).json({user})
+  const tokenUser = {name:user.name, userId:user._id, role:user.role}
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET , {expiresIn: process.env.JWT_LIFETIME})
+
+  res.status(StatusCodes.CREATED).json({user: tokenUser, token: token})
 }
 
 const login = async (req, res) => {
