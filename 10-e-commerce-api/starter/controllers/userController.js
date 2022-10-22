02d79456259469551ea49e4bf47c7ba80804d74f
jasuke-dev <ilham.suryaega@gmail.com>
2022-10-22 19:09:05 +0700

    showMe and updatePassword controller

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User')
-const customErrors = require('../errors')
+const CustomErrors = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const getAllUser = async (req, res) => {
   console.log(req.user);
@@ -12,18 +12,31 @@ const getSingleUser = async (req, res) => {
   console.log(userId);
   const user = await User.findOne({_id : userId}).select('-password')
   if(!user){
-    throw new customErrors.NotFoundError('No user with ID')
+    throw new CustomErrors.NotFoundError('No user with ID')
   }
   res.status(StatusCodes.OK).json({user})
 }
 const showCurrentUser = async (req, res) => {
-  res.send('who am i')
+  
+  res.status(StatusCodes.OK).json({user : req.user})
 }
 const updateUser = async (req, res) => {
   res.send('update user')
 }
 const updateUserPassword = async (req, res) => {
-  res.send('update password')
+  const {oldPassword, newPassword} = req.body
+  if(!oldPassword || !newPassword){
+    throw new CustomErrors.BadRequestError('Please provide Old and New Password')
+  }
+  const {userId} = req.user
+  const user = await User.findOne({_id : userId})
+  const oldPasswordValid = await user.comparePassword(oldPassword)
+  if(!oldPasswordValid){
+    throw new CustomErrors.UnauthenticatedError('Please valid credentials')
+  }
+  user.password = newPassword
+  await user.save()
+  res.status(StatusCodes.OK).json({msg : 'password updated'})
 }
 
 module.exports = {
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -15,10 +15,10 @@ const {
 
 router.route('/').get(authenticateUser, authorizePermission('admin','super'),getAllUser)
 
-router.route('/showMe').get(showCurrentUser)
+router.route('/showMe').get(authenticateUser,showCurrentUser)
 router.route('/updateUser').patch(authenticateUser,updateUser)
 
-router.route('/updateUserPassword').patch(updateUserPassword)
+router.route('/updateUserPassword').patch(authenticateUser,updateUserPassword)
 
 
 router.route('/:id').get(authenticateUser,getSingleUser)
